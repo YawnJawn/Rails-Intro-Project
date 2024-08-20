b52c2b71dfbe73e0f58218544f1ef57985d8fcb8
Tin <ttran79@rrc.ca>
2024-08-20 16:46:47 -0500

    Added show page and nav bar

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -2,4 +2,8 @@ class PokemonsController < ApplicationController
   def index
     @pokemons = Poke.includes(:image).page(params[:page]).per(10)
   end
+
+  def show
+    @pokemon = Poke.find(params[:id])
+  end
 end
